205f4b1dcf8286964a7f92884a11b1feca1d7cce
larpios <33772093+larpios@users.noreply.github.com>
2026-04-02 23:26:53 +0900

    fix: use correct end_col for nvim_buf_set_extmark

--- lua/jj-conflict/highlights.lua
+++ lua/jj-conflict/highlights.lua
@@ -41,17 +41,17 @@ function M.highlight_conflict(bufnr, conflict, config)
 		local line = vim.api.nvim_buf_get_lines(bufnr, i, i + 1, false)[1] or ""
 
 		if line:match("^<{7,}") or line:match("^>{7,}") then
-			vim.api.nvim_buf_set_extmark(bufnr, ns, i, 0, { hl_group = marker_hl, end_col = 0 })
+			vim.api.nvim_buf_set_extmark(bufnr, ns, i, 0, { hl_group = marker_hl, end_col = -1 })
 		elseif line:match("^%{7,}.*diff%s+from:") then
-			vim.api.nvim_buf_set_extmark(bufnr, ns, i, 0, { hl_group = ours_hl, end_col = 0 })
+			vim.api.nvim_buf_set_extmark(bufnr, ns, i, 0, { hl_group = ours_hl, end_col = -1 })
 		elseif line:match("^\\{7,}.*to:") then
-			vim.api.nvim_buf_set_extmark(bufnr, ns, i, 0, { hl_group = label_hl, end_col = 0 })
+			vim.api.nvim_buf_set_extmark(bufnr, ns, i, 0, { hl_group = label_hl, end_col = -1 })
 		elseif line:match("^%+{7,}") then
-			vim.api.nvim_buf_set_extmark(bufnr, ns, i, 0, { hl_group = theirs_hl, end_col = 0 })
+			vim.api.nvim_buf_set_extmark(bufnr, ns, i, 0, { hl_group = theirs_hl, end_col = -1 })
 		elseif line:match("^%-") then
-			vim.api.nvim_buf_set_extmark(bufnr, ns, i, 0, { hl_group = diff_rem_hl, end_col = 0 })
+			vim.api.nvim_buf_set_extmark(bufnr, ns, i, 0, { hl_group = diff_rem_hl, end_col = -1 })
 		elseif line:match("^%+") and not line:match("^%+{7,}") then
-			vim.api.nvim_buf_set_extmark(bufnr, ns, i, 0, { hl_group = diff_add_hl, end_col = 0 })
+			vim.api.nvim_buf_set_extmark(bufnr, ns, i, 0, { hl_group = diff_add_hl, end_col = -1 })
 		end
 	end
 end
